99b70dc90afea76709ce2dce089338e672f973d0
Carlos <carlos.braga@admcsc.com.br>
2019-04-22 02:20:30 -0300

    Update index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,10 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+  <q-page class="flex flex-center flex-block">
+    <q-img basic alt="ATS logo" src="statics/logo.png" contain />
+    <h2 class="text-center q-gutter-xs">ATS</h2>
+    <div class="full-width q-gutter-xs text-center">
+      <h4>Controle de mensalidades</h4>
+    </div>
   </q-page>
 </template>
 
